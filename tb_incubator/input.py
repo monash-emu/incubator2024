9f8547d597e5ff280ff3e0ff6913413e7facaf37
tyasarw <tyasarmw@gmail.com>
2024-08-28 11:57:42 +0700

    Add latency flow to the model

--- tb_incubator/input.py
+++ tb_incubator/input.py
@@ -2,6 +2,7 @@ import pandas as pd
 from tb_incubator.constants import set_project_base_path
 from summer2.functions.time import get_sigmoidal_interpolation_function
 from summer2 import Overwrite
+from summer2.parameters import Parameter
 
 
 project_paths = set_project_base_path("../tb_incubator")
@@ -339,6 +340,36 @@ def get_death_adjs(deathrate_df, age_strata):
         pop_death_func = get_sigmoidal_interpolation_function(years, rates)
         death_adjs[str(age)] = Overwrite(pop_death_func)
     return death_adjs
+
+# Add latency structures
+def add_latency_flow(model):
+    """
+    Adds latency flows to the compartmental model, representing the progression of the disease
+    through different stages of latency. This function defines three main flows: stabilization,
+    early activation, and late activation.
+
+    - Stabilization flow represents the transition of individuals from the 'early_latent' compartment
+      to the 'late_latent' compartment, indicating a period where the disease does not progress or show symptoms.
+
+    - Early activation flow represents the transition from 'early_latent' to 'infectious', modeling the
+      scenario where the disease becomes active and infectious shortly after the initial infection.
+
+    - Late activation flow represents the transition from 'late_latent' to 'infectious', modeling the
+      scenario where the disease becomes active and infectious after a longer period of latency.
+
+    Each flow is defined with a name, a rate (set to 1.0 and will be adjusted based on empirical data or model needs), and the source and destination compartments.
+
+    Args:
+        model: The compartmental model to which the latency flows are to be added.
+    """
+    latency_flows = [
+    ["stabilisation", "early latent", "late latent"],
+    ["early activation", "early latent", "infectious"],
+    ["late activation", "late latent", "infectious"],
+    ]
+    
+    for flow, source, dest in latency_flows:
+        model.add_transition_flow(flow, Parameter(f"{flow} rate"), source, dest)
 
 def display_summary(birth_data, pop_death_data):
     """
