b346f96c0e854569471f1337f8f72dc2f5547e18
tyasarw <tyasarmw@gmail.com>
2024-08-29 11:36:21 +0700

    Add infection flow to the model

--- tb_incubator/model.py
+++ tb_incubator/model.py
@@ -8,25 +8,6 @@ data_path = project_paths["DATA_PATH"]
 
 # Add latency structures
 def add_latency_flow(model):
-    """
-    Adds latency flows to the compartmental model, representing the progression of the disease
-    through different stages of latency. This function defines three main flows: stabilization,
-    early activation, and late activation.
-
-    - Stabilization flow represents the transition of individuals from the 'early_latent' compartment
-      to the 'late_latent' compartment, indicating a period where the disease does not progress or show symptoms.
-
-    - Early activation flow represents the transition from 'early_latent' to 'infectious', modeling the
-      scenario where the disease becomes active and infectious shortly after the initial infection.
-
-    - Late activation flow represents the transition from 'late_latent' to 'infectious', modeling the
-      scenario where the disease becomes active and infectious after a longer period of latency.
-
-    Each flow is defined with a name, a rate (set to 1.0 and will be adjusted based on empirical data or model needs), and the source and destination compartments.
-
-    Args:
-        model: The compartmental model to which the latency flows are to be added.
-    """
     latency_flows = [
     ["stabilisation", "early latent", "late latent"],
     ["early activation", "early latent", "infectious"],
@@ -39,3 +20,15 @@ def add_latency_flow(model):
     description= "We added latency flows to the compartmental model, representing the progression of the disease through different stages of latency. This function defines three main flows: stabilization, early activation, and late activation."
 
     return description
+
+def add_infection_flow(model):
+    infection_origins = ["susceptible", "late latent", "recovered"]
+
+    for origin in infection_origins:
+        model.add_infection_frequency_flow(f"infection_from_{origin}", Parameter("contact rate"), 
+                                           origin, "early latent")
+        
+    description= "We added infection flows to the model."
+
+    return description
+    
