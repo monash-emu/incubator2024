02c9bd93255408e97773eba27194c14b456f54f3
jtrauer <james.trauer@monash.edu>
2024-09-09 22:27:47 +1000

    Simplify some of the output generation

--- tb_incubator/outputs.py
+++ tb_incubator/outputs.py
@@ -15,28 +15,17 @@ def request_model_outputs(
         model.request_output_for_compartments(f"comp_size_{c}", c)
 
     # calculate and request percentage of latent population
-    model.request_output_for_compartments("total_population", compartments, save_results=False)
-    model.request_output_for_compartments(
-        "latent_population_size", latent_compartments, save_results=False
-    )
-    percentage_latent = DerivedOutput("latent_population_size") / DerivedOutput("total_population")
-    model.request_function_output("percentage_latent", percentage_latent)
+    tot_pop = model.request_output_for_compartments("total_population", compartments, save_results=False)
+    latent_pop = model.request_output_for_compartments("latent_pop_size", latent_compartments, save_results=False)
+    model.request_function_output("percentage_latent", latent_pop / tot_pop * 100.0)
 
     # death
-    model.request_output_for_flow("mortality_infectious", "TB death")
-    model.request_function_output(
-        "mortality",
-        1e5 * DerivedOutput("mortality_infectious") / DerivedOutput("total_population"),
-    )
+    deaths = model.request_output_for_flow("mortality_infectious", "TB death")
+    model.request_function_output("mortality", 1e5 * deaths / tot_pop)
 
     # total prevalence
-    model.request_output_for_compartments(
-        "infectious_population_size", infectious_compartments, save_results=False
-    )
-    prevalence_infectious = (
-        DerivedOutput("infectious_population_size") / DerivedOutput("total_population") * 1e5
-    )
-    model.request_function_output("prevalence", prevalence_infectious)
+    infect_pop = model.request_output_for_compartments("infectious_pop_size", infectious_compartments, save_results=False)
+    model.request_function_output("prevalence", infect_pop / tot_pop * 1e5)
 
     # incidence
     for timing in ["early", "late"]:
@@ -44,23 +33,19 @@ def request_model_outputs(
             f"{timing}_activation", f"{timing} activation", save_results=False
         )
 
-    model.request_aggregate_output(
+    inc_raw = model.request_aggregate_output(
         "incidence_raw", ["early_activation", "late_activation"], save_results=True
     )
 
-    incidence = 1e5 * DerivedOutput("incidence_raw") / DerivedOutput("total_population")
-    model.request_function_output("incidence", incidence)
+    model.request_function_output("incidence", 1e5 * inc_raw / tot_pop)
 
     # notification
-    model.request_function_output(
+    notifs = model.request_function_output(
         "notification", DerivedOutput("incidence_raw") * Parameter("case_detection_rate")
     )
 
     # case notification rate:
-    model.request_function_output(
-        "case_notification_rate",
-        DerivedOutput("notification") / DerivedOutput("incidence_raw") * 100,
-    )
+    model.request_function_output("case_notification_rate", notifs / DerivedOutput("incidence_raw") * 100.0)
 
     # request total population by age stratum
     for age_stratum in age_strata:
