6239064fecd746a6af204f33a54be6b41224be88
tyasarw <tyasarmw@gmail.com>
2024-10-03 19:31:30 +0700

    Adjust the priors for optimisation

--- tb_incubator/calibrate.py
+++ tb_incubator/calibrate.py
@@ -38,19 +38,19 @@ def get_all_priors() -> List:
         All the priors used under any analyses
     """
     priors = [
-        esp.UniformPrior("contact_rate", (0.1, 10.0)),
-        esp.UniformPrior("self_recovery_rate", (0.05, 0.50)),
-        esp.UniformPrior("screening_scaleup_shape", (0.01, 0.5)),
-        esp.UniformPrior("screening_inflection_time", (2000.0, 2018.0)),
-        esp.UniformPrior("time_to_screening_end_asymp", (0.1, 20.0)),
-        esp.UniformPrior("rr_infection_latent", (0.01, 1.0)),
-        esp.UniformPrior("rr_infection_recovered", (0.01, 1.0)),
-        esp.UniformPrior("seed_time", (1840.0, 1900.0)),
+        esp.UniformPrior("contact_rate", (8.0, 12.0)),
+        esp.UniformPrior("self_recovery_rate", (0.20, 1.00)),
+        #esp.UniformPrior("screening_scaleup_shape", (0.10, 0.5)),
+        #esp.UniformPrior("screening_inflection_time", (1990.0, 2018.0)),
+        #esp.UniformPrior("time_to_screening_end_asymp", (0.30, 20.0)),
+        #esp.UniformPrior("rr_infection_latent", (0.01, 1.0)),
+        #esp.UniformPrior("rr_infection_recovered", (0.01, 1.0)),
+        #esp.UniformPrior("seed_time", (1840.0, 1900.0)),
         #esp.UniformPrior("seed_duration", (1.0, 20.0)),
         #esp.UniformPrior("seed_rate", (1.0, 100.0)),
-        esp.UniformPrior("base_sensitivity", (0.01, 1.0)),
-        esp.UniformPrior("genexpert_sensitivity", (0.01, 1.0)),
-        esp.UniformPrior("progression_multiplier", (0.5, 1.5))
+        esp.UniformPrior("base_sensitivity", (0.2, 1.0)),
+        esp.UniformPrior("genexpert_sensitivity", (0.8, 1.0)),
+        esp.UniformPrior("progression_multiplier", (1.4, 1.7))
     ]
 
     return priors
